4a9dee5350666f2347f7412eeb196de0237dbd09
DESKTOP-BN9TJN4\Gabriel <gabriel.duarte16@hotmail.com>
2024-02-19 19:34:53 -0300

    feat: criação paginação e image

---
+++ Handcom.Web/Model/Extensions/ImageFile.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Handcom.Web.Model.Extensions
+{
+    public class ImageFile
+    {
+        [Required(ErrorMessage = "A Imagem é obrigatório.")]
+        public string Base64 { get; set; } = null!;
+
+        [Required(ErrorMessage = "O Nome do arquivo é obrigatório.")]
+        public string FileName { get; set; } = null!;
+
+    }
+}
---
+++ Handcom.Web/Pagination/Base/Pageable.cs
@@ -0,0 +1,12 @@
+﻿namespace Handcom.Web.Pagination.Base
+{
+    public class Pageable
+    {
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public string? Search { get; set; }
+        public string? Sort { get; set; }
+        public int? Direction { get; set; } 
+    }
+   
+}
