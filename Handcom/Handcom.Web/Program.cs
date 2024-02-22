39b5a16ee83a371202ca9560a84ee9fdb62b5e6f
DESKTOP-BN9TJN4\Gabriel <gabriel.duarte16@hotmail.com>
2024-02-22 00:29:47 -0300

    style: retirada de comentarios

--- Handcom/Handcom.Web/Program.cs
+++ Handcom/Handcom.Web/Program.cs
@@ -1,13 +1,7 @@
-using Blazored.LocalStorage;
 using Handcom.Web;
 using Handcom.Web.Configuration;
-using Handcom.Web.Services.Authentication;
-using Handcom.Web.Services.Interface;
-using Handcom.Web.Services.Services;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Radzen;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
